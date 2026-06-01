7b5c11102c25ab449a386d64fb0c9c80345c7fdc
Gopal Gupta <172278142+gopal092003@users.noreply.github.com>
2026-06-01 11:33:38 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./styles/index.css";
+import "./styles/index.css?test";
 
 ReactDOM.createRoot(
   document.getElementById("root")
