520d7341ea9efe9177ab2a3bf23936580b0f722c
Gopal Gupta <172278142+gopal092003@users.noreply.github.com>
2026-06-01 10:51:15 +0530

    Update Input.jsx

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -2,96 +2,85 @@ import { forwardRef } from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 
-const Input = forwardRef(({
-  label,
-  name,
-  type = "text",
-  placeholder = "",
-  value,
-  onChange,
-  onBlur,
-  error,
-  helperText,
-  required = false,
-  disabled = false,
-  fullWidth = true,
-  className,
-  ...props
-}, ref) => {
-  return (
-    <div
-      className={clsx(
-        "input-group",
-        {
-          "input-group--full":
-            fullWidth,
-        }
-      )}
-    >
-      {label && (
-        <label
-          htmlFor={name}
-          className="input-label"
-        >
-          {label}
-
-          {required && (
-            <span className="input-required">
-              *
-            </span>
-          )}
-        </label>
-      )}
-
-      <input
-        ref={ref}
-        id={name}
-        name={name}
-        type={type}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        onBlur={onBlur}
-        disabled={disabled}
+const Input = forwardRef(
+  (
+    {
+      label,
+      type = "text",
+      placeholder = "",
+      error,
+      helperText,
+      required = false,
+      disabled = false,
+      fullWidth = true,
+      className,
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div
         className={clsx(
-          "input",
+          "input-group",
           {
-            "input--error":
-              Boolean(error),
-          },
-          className
+            "input-group--full":
+              fullWidth,
+          }
         )}
-        {...props}
-      />
+      >
+        {label && (
+          <label
+            htmlFor={props.name}
+            className="input-label"
+          >
+            {label}
 
-      {error ? (
-        <span className="input-error">
-          {error}
-        </span>
-      ) : (
-        helperText && (
-          <span className="input-helper">
-            {helperText}
+            {required && (
+              <span className="input-required">
+                *
+              </span>
+            )}
+          </label>
+        )}
+
+        <input
+          ref={ref}
+          type={type}
+          placeholder={placeholder}
+          disabled={disabled}
+          className={clsx(
+            "input",
+            {
+              "input--error":
+                Boolean(error),
+            },
+            className
+          )}
+          {...props}
+        />
+
+        {error ? (
+          <span className="input-error">
+            {error}
           </span>
-        )
-      )}
-    </div>
-  );
-});
+        ) : (
+          helperText && (
+            <span className="input-helper">
+              {helperText}
+            </span>
+          )
+        )}
+      </div>
+    );
+  }
+);
 
 Input.displayName = "Input";
 
 Input.propTypes = {
   label: PropTypes.string,
-  name: PropTypes.string,
   type: PropTypes.string,
   placeholder: PropTypes.string,
-  value: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
-  onChange: PropTypes.func,
-  onBlur: PropTypes.func,
   error: PropTypes.string,
   helperText: PropTypes.string,
   required: PropTypes.bool,
